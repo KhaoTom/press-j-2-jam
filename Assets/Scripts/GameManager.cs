ed86358eac99496da100ba77d93867b72333e6cc
Edwin DeNicholas <disruption999@hotmail.com>
2021-05-17 22:33:36 -0700

    Map J key to increased happiness.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
     private int happiness;
 
     public int Happiness { get => happiness; }
+
+    public void OnJPressed(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+            IncreaseHappiness();
+    }
 
     public void IncreaseHappiness()
     {
