c7287d15aa255cfc22778afd9f53a80d104742e4
Edwin DeNicholas <disruption999@hotmail.com>
2021-05-17 20:21:19 -0700

    Add GameManager script and test script.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static int happiness;
+
+    public static int Happiness { get => happiness; }
+
+    public static void AddHappiness()
+    {
+
+    }
+}
